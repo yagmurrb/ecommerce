aed8a533f7490206c191c56fb15fe6f9d4365fde
Yagmur Caglar <yagmurrbeyza@gmail.com>
2024-04-25 22:07:26 +0300

    footerHeaderRow

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./layouts/Header.jsx";
+import Footer from "./layouts/Footer.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Header />
+      <App />
+      <Footer />
+    </BrowserRouter>
+  </React.StrictMode>
+);
