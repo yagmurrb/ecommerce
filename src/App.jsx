393237db7221a11dbdc196b589be85fa5ca37d37
Yagmur Caglar <yagmurrbeyza@gmail.com>
2024-04-26 14:21:37 +0300

    starting carousel

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import "./App.css";
 import HomePage from "./pages/HomePage";
 import BuyNow from "./components/BuyNow";
 import Featured from "./components/Featured";
-
+import GreenCarousel from "./components/GreenCarousel";
 function App() {
   return (
     <Switch>
       <Route exact path="/">
         <HomePage />
+        <GreenCarousel />
         <BuyNow />
+
         <Featured />
       </Route>
     </Switch>
